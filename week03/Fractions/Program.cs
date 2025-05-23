195394436f44af6c8825cb6ac32c64e1c5fe1d66
lightwisp <sayerdt@byuo.edu>
2025-05-22 19:00:24 -0600

    did the learning activity with Fraction

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -1,9 +1,34 @@
 using System;
+using System.Reflection.Metadata.Ecma335;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Fractions Project.");
+
+        Fraction fraction01 = new Fraction();
+        Fraction fraction02 = new Fraction(6);
+        Fraction fraction03 = new Fraction(5, 2);
+
+        fraction01.SetTop(1);
+        fraction01.SetBottom(1);
+        Console.WriteLine(fraction01.GetFractionString());
+        Console.WriteLine(fraction01.GetDecimalValue());
+
+        fraction01.SetTop(5);
+        fraction01.SetBottom(1);
+        Console.WriteLine(fraction01.GetFractionString());
+        Console.WriteLine(fraction01.GetDecimalValue());
+
+        fraction01.SetTop(3);
+        fraction01.SetBottom(4);
+        Console.WriteLine(fraction01.GetFractionString());
+        Console.WriteLine(fraction01.GetDecimalValue());
+
+        fraction01.SetTop(1);
+        fraction01.SetBottom(3);
+        Console.WriteLine(fraction01.GetFractionString());
+        Console.WriteLine(fraction01.GetDecimalValue());
     }
 }
