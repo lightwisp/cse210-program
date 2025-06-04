f4fe70208795191ae735b532e488ab4ebcf7f98b
lightwisp <sayerdt@gmail.com>
2025-06-03 22:24:46 -0600

    debuged Listing

--- week05/Mindfulness/Listing.cs
+++ week05/Mindfulness/Listing.cs
@@ -4,20 +4,20 @@ public class Listing : Mindfulness
     private List<string> _prompts = new List<string>();
     public Listing(string name, string description, int length) : base(name, description, length)
     {
-        _prompts.AddRange("Who are people that you appreciate?",
+        _prompts.AddRange(new List<string> {"Who are people that you appreciate?",
             "What are personal strengths of yours?",
             "Who are people that you have helped this week?",
             "When have you felt the Holy Ghost this month?",
-            "Who are some of your personal heroes?")
+            "Who are some of your personal heroes?" });
     }
 
     public void DisplayListing()
     {
-        ReadyMessage()
-        DisplayString(_prompts)
-        Menus.WaitingAnimation(10)
-        Menus.PrintSlow("Share your thoughts as quick as you can.")
-        DateTime start = DateTime.Now();
+        ReadyMessage();
+        DisplayString(_prompts);
+        Menus.WaitingAnimation(10);
+        Menus.PrintSlow("Share your thoughts as quick as you can.");
+        DateTime start = DateTime.Now;
         DateTime end = start.AddSeconds(GetActivityDuration());
         int i = 0;
         while (DateTime.Now < end)
@@ -25,6 +25,7 @@ public class Listing : Mindfulness
             Console.ReadLine();
             i++;
         }
-        Menus.PrintSlow($"You made {i} enteries in {GetActivityDuration()}/seconds")
+        Menus.PrintSlow($"You made {i} enteries in {GetActivityDuration()}/seconds");
+        Thread.Sleep(2000);
     }
 }
