f02be62dd4f8c16a9d7ea32036c96cc0b15ac7d4
lightwisp <sayerdt@byuo.edu>
2025-06-05 11:12:40 -0600

    added extra stuff i did in programs header

--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -2,6 +2,12 @@ using System;
 
 class Program
 {
+    // the etra stuf i added outside of requierments. i added a text animation for all descriptions.
+    // like a type writer afect.
+    // i added a Menu class that holds all of the functiuons needed for animations, genarice text diolog,
+    // and checks to see if the user typs in the corect switch case.
+    // probably could have writen this in Program but i think it looks cleaner.
+    // also added a uique waiting animations that uses "\r" + List to clear the line and then write over it.
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Mindfulness Project.");
